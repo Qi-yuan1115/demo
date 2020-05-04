a1484803feb92f936a3a290d0e43b49e29a3dd7b
Qi-yuan1115 <1348694033@qq.com>
2020-05-04 17:10:36 +0800

    hell world

---
+++ HelloWorld.cpp
@@ -0,0 +1,10 @@
+#include<iostream> //标准输入输出流库
+using namespace std;//c++命名空间
+int main()  //c++主函数,是程序执行的起点
+{
+    cout << "Hello world!" << endl; 
+   cout << "yuanqi!" << endl; 
+   cout << "world!" << endl; 
+   cout << "nihao!" << endl; 
+    return 0; //return 通常用于函数中,作为一个函数返回值!
+}
